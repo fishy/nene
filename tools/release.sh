43bd0ae813038847ae4f650b04db90ab21acade9
Yuxuan 'fishy' Wang <wang@yuxuan.org>
2022-05-21 18:55:53 -0700

    Handle release build for google play

--- tools/release.sh
+++ tools/release.sh
@@ -2,4 +2,13 @@
 
 set -e
 
-bazel build --config=release //:app
+keyfile=${1:-"${HOME}/.android/upload.jks"}
+
+target="release"
+output="app.aab"
+tempfile=$(mktemp -u app.tmp.aab.XXXXXXXXXX)
+
+bazel build --config=release ":${target}"
+cp "bazel-bin/${target}.aab" "${tempfile}"
+jarsigner -keystore "${keyfile}" "${tempfile}" upload
+mv "${tempfile}" "${output}"
